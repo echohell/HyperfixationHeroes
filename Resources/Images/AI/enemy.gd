7823005b643bab0af580fed16f50c91b70886e7c
echohell <59969944+echohell@users.noreply.github.com>
2024-01-17 17:22:28 -0400

    fixed bug that had collision not working
    fixed bug that had collision not working on enemy sprite for damage clicks

--- Resources/Images/AI/enemy.gd
+++ Resources/Images/AI/enemy.gd
@@ -1,19 +1,23 @@
 extends CharacterBody2D
 
 var DAMAGEPOPUP = preload("res://damage.tscn")
+var pressed = false
 
 func _input(event):
-	var damage = round(randf_range(10,20))
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		if get_viewport_rect().has_point(to_local(event.position)):
-			print(damage)
-			var text = DAMAGEPOPUP.instantiate()
-			text.amount = damage
-			add_child(text)
+	if (pressed == true):
+		var damage = round(randf_range(10,20))
+		if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+			if get_viewport_rect().has_point(to_local(event.position)):
+				print(damage)
+				var text = DAMAGEPOPUP.instantiate()
+				text.amount = damage
+				add_child(text)
 			
 			
 
 
+func _on_area_2d_mouse_exited():
+	pressed = false
 
-
-
+func _on_area_2d_mouse_entered():
+	pressed = true
