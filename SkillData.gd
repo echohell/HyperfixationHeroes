da2b5a873e885ed5b04c2972c29be87d45754aed
stringstrider <camdon.buckley90@gmail.com>
2024-03-26 13:58:20 -0300

    skill definitions
    T1 job definitions for warrior/mage/aspirant

--- SkillData.gd
+++ SkillData.gd
@@ -5,7 +5,8 @@ class_name skillDefinitions                                  # definition later 
 # if we need to add more things here like a reticle, etc. etc. it will update all skills
 
 @export var name: String                                                                     # name
-@export var skill_base: String                                                      # the base calc
+@export var skill_base: String   
+@export var skill_desc: String  #Cam Edit                                                 # the base calc
 @export var min_range: int                                                          # min max range
 @export var max_range: int
 @export var min_damage: int                                                        # min max damage
