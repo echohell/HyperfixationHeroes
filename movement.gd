e8afc9d422b560d4ba96d578c7b20ca1ba22a81d
echohell <59969944+echohell@users.noreply.github.com>
2024-08-28 13:10:34 -0300

    added movement to in_house with animations from sprite sheet
    added movement to in_house with animations from sprite sheet

--- movement.gd
+++ movement.gd
@@ -8,8 +8,11 @@ var stored_vel = Vector2(0,0)
 @export var inv: Control
 @export var nav: NavigationAgent2D
 
+@onready var AnimSprite = $Sprite2D2
+
 func _physics_process(delta):
 	if nav.is_navigation_finished():
+		AnimSprite.play("idle")
 		return
 	
 	if inv.visible == false:
@@ -19,8 +22,20 @@ func _physics_process(delta):
 	
 		var vel: Vector2 = next_path_pos - current_agent_pos
 		vel = vel.normalized()
+		
+		if vel.x > 0 and (vel.y <= .65 and vel.y >= -.65):
+			AnimSprite.play("walk_right")
+		elif vel.x < 0 and (vel.y <= .65 and vel.y >= -.65):
+			AnimSprite.play("walk_left")
+		elif vel.y < 0 and (vel.x < .66 and vel.x > -.66):
+			AnimSprite.play("walk_up")
+		elif vel.y > 0 and (vel.x < .66 and vel.x > -.66):
+			AnimSprite.play("walk_down")
+		if vel.x == 0 and vel.y == 0:
+			AnimSprite.play("idle")
 	
 		velocity = vel.lerp(vel * speed, accel * delta)
+			
 		move_and_slide()
 
 func _unhandled_input(event: InputEvent) -> void:
